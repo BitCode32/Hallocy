096fe9a95d2dce80f0c6b8b26a0686dc370d962e
BitCode32 <164204550+BitCode32@users.noreply.github.com>
2025-02-03 10:15:30 +0100

    feat(docs): added license and description at beginning of every code file

--- Src/Main.c
+++ Src/Main.c
@@ -1,3 +1,25 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * -----------------------------------------------------------------------------
+ * File: Main.c
+ * Description:
+ *  This file is used as an example of how to use the library. When compiling 
+ *  this file is used to create the HallocyApp.exe file.
+ * 
+ * Author: BitCode32
+ * -----------------------------------------------------------------------------
+ */
 #include <Hallocy/Allocator.h>
 #include <stdio.h>
 
